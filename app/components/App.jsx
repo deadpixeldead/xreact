83791bfb0d01ee11ff7d03398ebb74520c34aee9
awitherow <au.witherow@gmail.com>
2016-01-29 10:41:35 +0100

    updated scss structure

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
-import Hello from './hello/hello.jsx';
 
 export default class App extends React.Component {
 	render() {
-		return <Hello />;
+		return <p>Hello World!</p>;
 	}
 }
